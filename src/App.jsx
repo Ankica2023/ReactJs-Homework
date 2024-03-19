c82483a4d4f4a2e5816d49b77c89864fd94da4dd
Ankica2023 <ankica.n@yahoo.com>
2024-03-19 22:06:33 +0100

    D1

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { GreetingPage } from "./components/GreetingPage";
+
+
+export function App() {
+  return(
+    <React.Fragment>
+        <GreetingPage/>
+    </React.Fragment>
+  )
+}
---
+++ src/components/GreetingPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./../assets/css/GreetingPage.css";
+
+export const GreetingPage = () => {
+    return (
+        <div id="greeting"> 
+        <div id="content">
+           <h1 ClassName="header">Hello dear User</h1>
+
+           <p ClassName="message">Welcome to our app</p>
+        </div>
+
+        </div>
+    )
+}
